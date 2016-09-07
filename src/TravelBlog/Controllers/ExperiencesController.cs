7b70e0548218fe968bf58fb442918717d5f452e0
Joel Delight and Wolfgang Warneke <joel+wolfgang@>
2016-09-07 16:25:52 -0700

    bringing that CRUD functionality to LOCATIONS

--- src/TravelBlog/Controllers/ExperiencesController.cs
+++ src/TravelBlog/Controllers/ExperiencesController.cs
@@ -14,6 +14,7 @@ namespace TravelBlog.Controllers
         public IActionResult Index()
         {
             return View();
+            
         }
     }
 }
--- src/TravelBlog/Controllers/LocationsController.cs
+++ src/TravelBlog/Controllers/LocationsController.cs
@@ -19,5 +19,53 @@ namespace TravelBlog.Controllers
         {
             return View(db.Locations.ToList());
         }
+
+        public IActionResult Details(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            db.Locations.Add(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Location location)
+        {
+            db.Entry(location).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations .LocationId == id);
+            db.Locations.Remove(thisLocation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
